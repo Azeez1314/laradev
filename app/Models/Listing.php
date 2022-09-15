32bbddd05402fc362e3c03681af5e986eb45f0f0
Azeez Roheem <mediatoor@gmail.com>
2022-09-15 19:28:35 +0100

    additional commit

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,36 +1,11 @@
 <?php
-    namespace App\Models;
 
-    class Listing {
-        public static function all() {
-            return [
-                [
-                    'id' => 1,
-                    'title' => 'Listing One',
-                    'description' => 'where you can register web routes for your application. These
-                    | routes are loaded by the RouteServiceProvider within a group which
-                    | contains the "web" middleware group. Now create',
-                ],
-                [
-                    'id' => 2,
-                    'title' => 'Listing Two',
-                    'description' => 'where you can register web routes for your application. These
-                    | routes are loaded by the RouteServiceProvider within a group which
-                    | contains the "web" middleware group. Now create',
-                ]
-            ];
-        }
+namespace App\Models;
 
-        public static function find($id) {
-            $listings = self::all()
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-            foreach ($listings as $listing) {
-                if($listing['id'] == $id) {
-                    return $listing;
-                }
-            }
-        }
-    }
-    
-
-?>
+class Listing extends Model
+{
+    use HasFactory;
+}
